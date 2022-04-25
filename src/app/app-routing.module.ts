92bc6b42f2e2e88197bde19deb870d0faaaaebec
dhonstudio <doon13@gmail.com>
2022-04-25 07:38:02 +0700

    Create index and auth component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+import { IndexComponent } from './components/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: IndexComponent,
+    // canActivate: [AuthGuardService]
+  },
+  {
+    path: 'auth', 
+    component: AuthComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { AuthComponent } from './components/auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
